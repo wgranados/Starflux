da923d7b09a930bdcafc233452d56304e4fd6c73
William Granados <me@wgma00.me>
2017-07-05 21:43:57 -0400

    added some other modules

---
+++ hexdecoder.v
@@ -0,0 +1,72 @@
+// BSD 3-Clause License
+// 
+// Copyright (c) 2017, William Granados, Saskia Tjioe, Venkada Naraisman Prasad
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// 
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+module hex_decoder_always(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
+endmodule
+
+module hex_decoder(A,B,C,D,H);
+    input A;
+    input B;
+    input C;
+    input D;
+    output [6:0]H;
+
+    assign H[0] = (B | D) & (A | ~C) & (~B | ~C) & (~A | D) & (A | ~B | ~D) & (~A | B | C);
+    assign H[1] = (A | B) & (B | D) & (A | C | D) & (A | ~C | ~D) & (~A | C | ~D);
+    assign H[2] = (A | C) & (A | ~D) & (C | ~D) & (A | ~B) & (~A | B);
+    assign H[3] = (~A | C) & (A | B | D) & (B | ~C | ~D) & (~B | C | ~D) & (~B | ~C | D);
+    assign H[4] = (B | D) & (~C | D) & (~A | ~C) & (~A | ~B); 
+    assign H[5] = (C | D) & (~B | D) & (~A | B) & (~A | ~C) & (A | ~B | C);
+    assign H[6] = (B | ~C) & (~C | D) & (~A | B) & (~A | ~D) & (A | ~B | C);
+
+endmodule
